6321ec94c784659de97f7383c9e280f709e4bab2
AlanSantiagoVasquezAriza <alanvasquez.sa@academia.umb.edu.co>
2025-09-26 09:37:40 -0500

    Añadir cors

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth.routes import router as auth_router
 from routes.users import router as users_router
 from routes.subjects import router as subjects_router
 from routes.notes import router as notes_router
 
+
 app = FastAPI(
     title="Asistente Inteligente de Estudio",
     description="Una API para gestionar usuarios y materias, facilitando el estudio mediante la organización de notas y tareas.",
     version="1.0.0",
+)
+
+# Habilitar CORS para cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
